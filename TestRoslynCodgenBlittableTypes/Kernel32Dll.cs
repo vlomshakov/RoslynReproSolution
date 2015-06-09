620926648306a1d860351fad882148823baab7a8
unknown <Vadim.Lomshakov@UNIT-321.Labs.IntelliJ.Net>
2015-06-09 16:25:40 +0300

    repro sln

---
+++ TestRoslynCodgenBlittableTypes/Kernel32Dll.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+namespace TestRoslynCodgenBlittableTypes
+{
+  [StructLayout(LayoutKind.Sequential)]
+  public struct FILE_TIME
+  {
+    public FILE_TIME(long fileTime)
+    {
+      ftTimeLow = (uint) fileTime;
+      ftTimeHigh = (uint) (fileTime >> 32);
+    }
+
+    [MethodImpl(0x100)]
+    public long ToTicks()
+    {
+      return ((long) ftTimeHigh << 32) + ftTimeLow;
+    }
+
+    [MethodImpl(0x100)]
+    public DateTime ToDateTimeUtc()
+    {
+      return DateTime.FromFileTimeUtc(ToTicks());
+    }
+
+    public uint ftTimeLow;
+    public uint ftTimeHigh;
+  }
+
+  public sealed class SafeFindHandle : SafeHandleZeroOrMinusOneIsInvalid
+  {
+    public SafeFindHandle() : base(true)
+    {
+    }
+
+    protected override unsafe bool ReleaseHandle()
+    {
+      return Kernel32Dll.FindClose((void*) handle) != 0;
+    }
+  }
+
+
+  [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+  public unsafe struct WIN32_FIND_DATA
+  {
+    public uint dwFileAttributes;
+    public FILE_TIME ftCreationTime;
+    public FILE_TIME ftLastAccessTime;
+    public FILE_TIME ftLastWriteTime;
+    public uint nFileSizeHigh;
+    public uint nFileSizeLow;
+    public uint dwReserved0;
+    public uint dwReserved1;
+    public fixed char cFileName [260];
+    public fixed char cAlternateFileName [14];
+  }
+
+  public static unsafe class Kernel32Dll
+  {
+    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, PreserveSig = true, SetLastError = true, ExactSpelling = true)
+    ]
+    public static extern SafeFindHandle FindFirstFileW(string lpFileName, WIN32_FIND_DATA* lpFindFileData);
+
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, PreserveSig = true, SetLastError = true, ExactSpelling = true)
+    ]
+    public static extern int FindClose(void* handle);
+  }
+}
---
+++ TestRoslynCodgenBlittableTypes/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace TestRoslynCodgenBlittableTypes
+{
+  public static class Test
+  {
+    private static unsafe void Main(string[] args)
+    {
+      var data = new WIN32_FIND_DATA();
+      using (var handle = Kernel32Dll.FindFirstFileW(@"C:\work\*", &data))
+      {
+        if (handle.IsInvalid)
+        {
+          var error = Marshal.GetLastWin32Error();
+          throw new IOException("IO error with code {0}.", error);
+        }
+      }
+      Console.WriteLine("Roslyn vs Csc: test - calling of P/Invoke with blittable stucture as argument.");
+    }
+  }
+}
